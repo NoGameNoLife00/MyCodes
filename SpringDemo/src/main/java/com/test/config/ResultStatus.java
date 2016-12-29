2579212e1cd33b4837f5e370aef755ec4ea2652d
NoGameNoLife <NoGameNo_Life@163.com>
2016-12-29 11:49:39 +0800

    统一成功码

--- SpringDemo/src/main/java/com/test/config/ResultStatus.java
+++ SpringDemo/src/main/java/com/test/config/ResultStatus.java
@@ -4,7 +4,7 @@ package com.test.config;
  * Created by Administrator on 2016/11/22.
  */
 public enum ResultStatus {
-    SUCCESS(100, "成功"),
+    SUCCESS(200, "成功"),
     USERNAME_OR_PASSWORD_ERROR(-1001, "用户名或密码错误"),
     USER_NOT_FOUND(-1002, "用户不存在"),
     USER_NOT_LOGIN(-1003, "用户未登录");
